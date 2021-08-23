f1886652f416c11d962929386b277423f73d0186
Alec Mekarzel <alec.mekarzel@gmail.com>
2021-08-23 19:10:25 -0400

    Update codebase to new `commander` package

--- src/command.js
+++ src/command.js
@@ -4,7 +4,7 @@
 
 const path = require('path')
 const spronk = require('./index')
-const program = require('commander')
+const { program } = require('commander')
 const package = require('../package.json')
 
 program
@@ -18,13 +18,14 @@ program
 	.option('-g, --grace <n>', 'ms grace period after worker SIGTERM [5000]', parseInt)
 	.option('-l, --lifetime <n>', 'ms to keep cluster alive if workers exit [Infinity]', parseInt)
 	.action(function(file) {
+		const options = program.opts()
 		const resolvedPath = path.resolve(file)
 
 		spronk({
 			start: function() { require(resolvedPath) },
-			workers: program.workers,
-			grace: program.grace,
-			lifetime: program.lifetime
+			workers: options.workers,
+			grace: options.grace,
+			lifetime: options.lifetime
 		})
 	})
 
